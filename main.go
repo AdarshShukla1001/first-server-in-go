23f4ec5b307260c0124c014a31e874a8596f5255
Adarsh Shukla <adarshshukla10012002@gmail.com>
2024-08-08 19:34:47 +0530

    Added first err and json handlers

--- main.go
+++ main.go
@@ -7,38 +7,63 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
-
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
-func main() {
-		godotenv.Load(".env")
 
-	portString:= os.Getenv("PORT")
-	
-	
+func respondWithError(w http.ResponseWriter,code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5XX error:")
+	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJson(w,code,errResponse{
+		Error: msg,
+	})
+}
+
+
+func main() {
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	router:= chi.NewRouter()
+	router := chi.NewRouter()
 
-	srv:= &http.Server{
-		Handler : router,
-		Addr: ":" + portString,
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	// On get request
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err",handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
 	}
 
-	log.Printf("Server starting on port %v",portString)
+	log.Printf("Server starting on port %v", portString)
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-
-
-
-
-
-
-	fmt.Println("Port:" , portString)
+	fmt.Println("Port:", portString)
 }

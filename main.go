3af9b0ec78eb9bdace931ba36b1679ae90daa0a1
Adarsh Shukla <adarshshukla10012002@gmail.com>
2024-08-08 18:42:21 +0530

    Added env and gitignore file

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Print("Hello, world!")
+	portString:= os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+		
+	}
 }
